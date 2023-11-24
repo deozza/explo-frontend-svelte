ebfec98b657d1b019eec5c9c2c65b61f9853be47
Edenn Touitou <edenn.touitou@letsjam.fr>
2023-11-24 09:00:52 +0000

    reordered

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,11 @@
 <script lang="ts">
     import '../main.css';
     import CounterButton from '../lib/CounterButton.svelte';
-    import { userToken } from '../store';
 
     let counter: number = 10;
 
     $: double = counter * 2;
 
-
-    let token: string | null = '' 
-
-    userToken.subscribe((value) => token = value )
 
 </script>
 
@@ -24,7 +19,6 @@
 <CounterButton bind:counter={counter} />
 
 <p>Double : {double}</p>
-<p>User token  : {token}</p>
 
 <style>
 
---
+++ src/routes/user/[id]/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+
+  let pokemon = null
+
+  async function searchPokemon(){
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+    const responseBody = await response.json()
+    pokemon = responseBody
+    console.log(responseBody)
+  }
+
+
+</script>
+
+<svelte:head>
+    <title>Profil du user {$page.params.id} </title>
+    <meta property="description" content="Bonjour ceci est mon site web" />
+</svelte:head>
+
+<p>User id  : {$page.params.id}</p>
+
+<button on:click={searchPokemon}>Click to search</button>
+
+<style>
+
+</style>
